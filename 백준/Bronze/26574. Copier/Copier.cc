1ed94f6f9e30ac70b3f448927c1298c447640c63
jinju-yang <129378360+jinju-yang@users.noreply.github.com>
2024-06-21 15:58:06 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <list>
+#define fio ios_base::sync_with_stdio(0), cin.tie(0)
+using namespace std;
+
+int main(void) {
+    int N;
+    cin >> N;
+    while (N--) {
+        int num;
+        cin >> num;
+        cout << num << " " << num << "\n";
+    }
+}
