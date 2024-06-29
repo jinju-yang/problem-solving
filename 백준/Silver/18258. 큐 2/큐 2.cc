4e0caa06ea76e2a6bbf0fea96c56cf8c863ae233
jinju-yang <129378360+jinju-yang@users.noreply.github.com>
2024-06-29 20:54:04 +0900

    [Silver IV] Title: 큐 2, Time: 328 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+
+	cin.tie(NULL);
+
+	cout.tie(NULL);
+
+	int N, X;
+	string cmd;
+	queue<int> q;
+	cin >> N;
+	while (N--) {
+		cin >> cmd;
+		if (cmd == "push") {
+			cin >> X;
+			q.push(X);
+		}
+		else if (cmd == "pop") {
+			if (q.size()) {
+				cout << q.front() << "\n";
+				q.pop();
+			}
+			else cout << -1 << "\n";
+		}
+		else if (cmd == "size") cout << q.size() << "\n";
+		else if (cmd == "empty") {
+			if (q.empty()) cout << 1 << "\n";
+			else cout << 0 << "\n";
+		}
+		else if (cmd == "front") {
+			if (q.size()) cout << q.front() << "\n";
+			else cout << -1 << "\n";
+		}
+		else if (cmd == "back") {
+			if (q.size()) cout << q.back() << "\n";
+			else cout << -1 << "\n";
+		}
+	}
+}
